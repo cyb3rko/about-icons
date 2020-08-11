0d53020d0a76cb3cf54cd935c7251e0c9f00cc8f
Cyb3rKo <niko@cyb3rko.de>
2020-08-11 23:32:11 +0200

    rewritten sample app to Kotlin

---
+++ app/src/main/java/com/cyb3rko/abouticonssample/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.cyb3rko.abouticonssample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cyb3rko.abouticons.AboutIcons
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(AboutIcons(this, R.drawable::class.java).get())
+    }
+}
