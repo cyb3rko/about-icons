8fa80c934d3b12c4ae24c151230c9b260cb405b5
Cyb3rKo <niko@cyb3rko.de>
2020-08-16 20:19:23 +0200

    addition to sample app

--- app/src/main/java/com/cyb3rko/abouticonssample/MainActivity.kt
+++ app/src/main/java/com/cyb3rko/abouticonssample/MainActivity.kt
@@ -7,6 +7,6 @@ import com.cyb3rko.abouticons.AboutIcons
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(AboutIcons(this, R.drawable::class.java).get())
+        setContentView(AboutIcons(this, R.drawable::class.java).setTitle("My Used Icons").get())
     }
 }
